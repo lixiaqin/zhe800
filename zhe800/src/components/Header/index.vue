d7f76685956af90e459b5458a7166e236da80045
lixiaqin <763948458@qq.com>
2017-08-04 10:27:27 +0800

    header2

--- zhe800/src/components/Header/index.vue
+++ zhe800/src/components/Header/index.vue
@@ -1,23 +1,49 @@
 <template>
    <div class="header-bar">
-     <slot name="headerLift"></slot>
-     <h2 class="title">{{title}}</h2>
-     <slot name="headerRight"></slot>
+    <div class="header-left">
+      <slot name="header-left"></slot>
+    </div> 
+    <div class="header-center">
+       <slot name="header-center"></slot>
+    </div>
+     <div class="header-right">
+       <slot name="header-right"></slot>
+    </div>
+    
    </div>
 </template>
 
 <script>
 export default {
-  name: 'Header',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  name: 'HeaderBar',
+  props:{
+    title:{
+      default:''
     }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
+<style scoped lang="less">
+.header-bar{
+  width:100%;
+  height:.44rem;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  position:relative;
+  border-bottom: .001rem solid #d9d9d9;
+  background-color: #fff;
+  padding:.05rem 0;
+  .header-left{
+    position:absolute;
+    left:.1rem;
+  }
+  .header-right{
+    position:absolute;
+    right:.05rem;
+    top:.05rem;
+  }
+}
 </style>
---
+++ zhe800/src/views/Home/index.vue
@@ -0,0 +1,43 @@
+<template>
+   <div class="home-page">
+     <HeaderBar>
+       
+        <div class="header-center" slot="header-center"></div> 
+        <div class="header-right" slot="header-right"></div> 
+     </HeaderBar>
+     
+   </div>
+</template>
+
+<script>
+import HeaderBar from '../../components/Header'
+export default {
+  name: 'home',
+  components:{
+     HeaderBar
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+
+.header-center{
+    width: .7rem;
+    height: .26rem;
+    background: url(//status.tuanimg.com/statics/mz/common/img/logo116x45.png) no-repeat 0 0;
+    background-size: .7rem .26rem;
+}
+.header-right{
+    width:.2rem;
+    height:.18rem;
+    background: url(//status.tuanimg.com/statics/mz/index/img/category-icon.png) no-repeat 0 0;
+    background-size: .2rem .18rem;
+    margin-top: .07rem;
+}
+</style>
