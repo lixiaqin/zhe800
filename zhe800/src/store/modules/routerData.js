9e233c8bfac0f9c59df4a5de9088b8bf2915be6f
lixiaqin <763948458@qq.com>
2017-08-09 16:37:40 +0800

    v8.9.4

--- zhe800/src/store/modules/routerData.js
+++ zhe800/src/store/modules/routerData.js
@@ -1,20 +1,21 @@
-const route={
-  namespaced: true,
-  state:{
-    routerData:[]
-  },
-  getters(state){
-    return state.routerData
-  },
-  mutations:{
-    SET_ROUTER(state,routerData){
-      state.routerData=routerData
+const route = {
+    namespaced: true,
+    state: {
+        routerData: []
+    },
+    getters(state) {
+        return state.routerData
+    },
+    mutations: {
+        SET_ROUTER(state, routerData) {
+            state.routerData = routerData
+        }
+    },
+    actions: {
+        setRouter({ commit }, routerData) {
+            commit('SET_ROUTER', routerData)
+            console.log(routerData)
+        }
     }
-  },
-  actions:{
-    setRouter({commit},routerData){
-      commit('SET_ROUTER',routerData)
-    }
-  }
 }
 export default route
--- zhe800/src/views/Classify/classif/index.vue
+++ zhe800/src/views/Classify/classif/index.vue
@@ -4,19 +4,33 @@
    <div class="classleft-page">
      <div class='class-item'>
         <swiper :options="swiperOption" ref="mySwiper">
-         <swiper-slide v-for="item in data">
-          <div>{{item.name}}</div>
-         </swiper-slide>
-          
+         <swiper-slide v-for="(item,index) in datas">
+          <div v-for='i in item.tag' v-if='i.level==1'  @click='tabToggle(index)'>{{i.category_name}}</div>
+         </swiper-slide>       
        </swiper>
-      <div class="swiper-scrollbar"></div>
+      
      </div>
-     
+     <div class='class-page-right'>
+        <swiper :options="swiperOption" ref="mySwiper">
+          <swiper-slide v-for="(item,indexs) in datas">
+               <div v-if='indexs==id'>
+                  <h2>为你推荐</h2>
+                  <img :src='item.banner[0].pic' class='class-banner'/>
+                  <h2>热门分类</h2>
+                <div v-for='i in item.tag' v-if='i.level==2'>
+                   <div class='hot-class'>
+                     <img :src='i.pic' />
+                     <p>{{i.category_name}}</p>
+                   </div>
+                </div>
+               </div>
+          </swiper-slide>       
+        </swiper>
+     </div>
    </div>
 </template>
 
 <script>
-
 export default {
   name: 'classif',
   data () {
@@ -29,42 +43,28 @@ export default {
           slidesPerView:'auto'
 	
         },
-        data:[{
-            name:'地方'
-        },{
-            name:'地方'
-        },{
-            name:'地方'
-        },{
-            name:'地方'
-        },{
-            name:'地方'
-        },{
-            name:'地方'
-        },{
-            name:'地方'
-        },{
-            name:'地方'
-        },{
-            name:'地方'
-        },{
-            name:'地方'
-        },{
-            name:'地方'
-        },{
-            name:'地方'
-        },{
-            name:'地方'
-        },{
-            name:'地方'
-        }]
+        datas:{},
+        id:1
     }
   },
-  components:{
-     
-   
-   
+  created(){
+      var that = this;
+      var promise = new Promise(function(resolve, reject) {
+      that.$axios.get('/classType').then(function (res) {
+         resolve(res.data)
+       })
+     }).then(function(data){
+       that.datas=data;
+        console.log(that.datas)
+     })
     
+      
+  },
+  methods:{
+    tabToggle(index){
+      this.id=index;
+      console.log(this.datas)
+    }
   }
 }
 </script>
@@ -77,19 +77,51 @@ body,html{
     overflow: hidden;
 }
 .classleft-page{
-    width: 100%; height: 100%;
+    width: 100%; height: 100%;display:flex;
+   display:flex;
 }
 .class-item{
-    width:.5rem;
-    height:500px;;
+    width:.9rem;
+    height:100%;;
     overflow:hidden;
     border:1px solid #ccc;
-    div{
-        padding:.1rem .05rem;
+    
+    .swiper-slide{
+        padding:.14rem .05rem;
+        text-align:center;
+        border-bottom:.01rem solid #ccc;
     }
 }
-.swiper-slide{
-   
+.class-page-right{
+      width:100%;
+      height:100%;
+      overflow:hidden;
+      h2{
+        height:.3rem;
+        line-height:.3rem;
+        padding-left:.2rem;
+        font-size:.14rem;
+        font-weight: bold;
+      }
+      .class-banner{
+        width:100%;
+      }
+      .hot-class{
+        width:30%;
+        float:left;
+        margin-left:3.3%;
+        margin-top:.1rem;
+        img{
+          width:75%;
+          margin-left:10%;
+        }
+        p{
+          width:100%;
+          text-align:center;
+        }
+      }
+     
 }
+
 </style>
 
